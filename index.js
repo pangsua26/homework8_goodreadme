d27baddbff20a7aec9b26c68e5db52adf04003eb
Pangsua Vang <pangsua26@yahoo.com>
2020-05-27 12:35:54 -0500

    added first prompt question and gitignore.txt

--- index.js
+++ index.js
@@ -3,5 +3,13 @@ const inquirer = require("inquirer");
 const util = require("util");
 const axios = require("axios");
 
+inquirer
+.promptUser([
+    {
+        type: "input",
+        name: "title",
+        message: "What is the project title?"
+    }
+])
 
 
